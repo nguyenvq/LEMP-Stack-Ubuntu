a9c30fe42b341a3b7020f1bc2daddfbfef3b4ded
bajula <alex@bnetzone.ro>
2017-09-20 12:07:07 +0300

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 # add php7 and nginx repos
 sudo add-apt-repository -y ppa:nginx/development
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
-
-
 # install basics
 sudo apt-get install -y git tmux vim curl wget zip unzip htop
 # Nginx
 sudo apt-get install -y nginx
 # PHP
 sudo apt-get install -y php7.1-fpm php7.1-cli php7.1-mcrypt php7.1-gd php7.1-mysql php7.1-pgsql php7.1-imap php-memcached php7.1-mbstring php7.1-xml php7.1-curl php7.1-bcmath php7.1-sqlite3 php7.1-xdebug
-
 # Composer
 php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-dir=/usr/bin/ --filename=composer
 echo "Installing Laravel into /var/www"
@@ -40,7 +36,26 @@ server {
     }
 }
 ' > /etc/nginx/sites-available/default 
-
 chown -R www-data: /var/www/myapp/storage /var/www/myapp/bootstrap
 #Restart Nginx
     service nginx restart
+
+
+############### Setting for PHP
+bash config-php71.sh
+bash config-mysql.sh 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
